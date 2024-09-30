7a4f6b23b03ed27390f647a2bcb039b405119b4f
nunezh <nunezh@sunypoly.edu>
2024-09-30 16:55:56 -0400

    project 01 initial files

---
+++ sample__db_create.sql
@@ -0,0 +1,31 @@
+CREATE TABLE course
+(
+  crn   INT NOT NULL,
+  title    ,
+  PRIMARY KEY (crn)
+);
+
+CREATE TABLE student 
+(
+  uid        INT     NOT NULL,
+  first_name varchar NOT NULL,
+  PRIMARY KEY (uid)
+);
+
+CREATE TABLE student_course
+(
+  uid INT NOT NULL,
+  cid INT NOT NULL,
+  crn INT NOT NULL,
+  PRIMARY KEY (uid)
+);
+
+ALTER TABLE student_course
+  ADD CONSTRAINT FK_student _TO_student_course
+    FOREIGN KEY (uid)
+    REFERENCES student  (uid);
+
+ALTER TABLE student_course
+  ADD CONSTRAINT FK_course_TO_student_course
+    FOREIGN KEY (crn)
+    REFERENCES course (crn);
