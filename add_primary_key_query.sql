bb309d000d61888dc4df2cb2388f5fb07488f838
nunezh-sunypoly <131188638+nunezh-sunypoly@users.noreply.github.com>
2024-11-06 21:05:00 -0500

    Update add_primary_key_query.sql

--- add_primary_key_query.sql
+++ add_primary_key_query.sql
@@ -5,10 +5,10 @@ LANGUAGE plpgsql
 as $$
 	begin
 
-			if NOT exists (select constraint_name from information_schema.table_constraints where table_name = 'occupations' and constraint_type = 'PRIMARY KEY') then
+		if NOT exists (select constraint_name from information_schema.table_constraints where table_name = 'occupations' and constraint_type = 'PRIMARY KEY') then
 
 			ALTER TABLE occupations
-  				ADD PRIMARY KEY (occupation_id);
+  			ADD PRIMARY KEY (occupation_id);
 
 		end if;
 
