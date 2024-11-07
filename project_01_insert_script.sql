35a1af3ebdc2de6c27a6b3b4437004c8708621b9
nunezh-sunypoly <131188638+nunezh-sunypoly@users.noreply.github.com>
2024-11-06 21:02:37 -0500

    Update project_01_insert_script.sql

--- project_01_insert_script.sql
+++ project_01_insert_script.sql
@@ -9,10 +9,10 @@ INSERT INTO state_data (state_code, state_name) VALUES
 
 -- Insert into employment_data
 INSERT INTO employment_data (occupation_id, year, state_id, employment, employment_rse) VALUES
-(1, CONVERT(date,'22.12.2023',104), 1, 50000, 1.5), -- California
-(1, CONVERT(date,'07.06.2015',104), 2, 30000, 1.7); -- Texas
+(1, to_date('19-09-2022', 'DD-MM-YYYY'), 1, 50000, 1.5), -- California
+(1, to_date('11-06-2020', 'DD-MM-YYYY'), 2, 30000, 1.7); -- Texas
 
 -- Insert into wages
 INSERT INTO wages (occupation_id, year, state_id, hourly_mean_wage, annual_mean_wage, wage_rse) VALUES
-(1, CONVERT(date,'11.09.2013',104), 1, 50.75, 105560, 1.2), -- California
-(1, CONVERT(date,'03.04.2003',104), 2, 48.20, 100256, 1.4); -- Texas
+(1, to_date('19-09-2022', 'DD-MM-YYYY'), 1, 50.75, 105560, 1.2), -- California
+(1, to_date('11-06-2020', 'DD-MM-YYYY'), 2, 48.20, 100256, 1.4); -- Texas
