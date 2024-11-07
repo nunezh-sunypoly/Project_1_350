edc471cefe4d43516407439ebb8203808ea15645
NicholasLittlefield <143556001+NicholasLittlefield@users.noreply.github.com>
2024-11-06 19:35:16 -0500

    Uploaded a primary key query table

---
+++ add_primary_key_query.sql
@@ -0,0 +1,14 @@
+CREATE or REPLACE FUNCTION add_prim_key() RETURNS void
+LANGUAGE plpgsql
+as $$
+	begin
+
+			if NOT exists (select constraint_name from information_schema.table_constraints where table_name = 'occupations' and constraint_type = 'PRIMARY KEY') then
+
+			ALTER TABLE occupations
+  				ADD PRIMARY KEY (occupation_id);
+
+		end if;
+
+	end;
+$$;
